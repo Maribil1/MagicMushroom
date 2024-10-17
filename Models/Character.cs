14ce1af382977d475e1ad3cd13bbe9edcc316599
opilane <opilane@ORDI-402560>
2024-10-17 13:35:28 +0300

    Add project files.

---
+++ Models/Character.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicMushroom.Models
+{
+    public class Character 
+    {
+        [Required]
+        public string CharacterName { get; set; }
+        [Required]
+        public string CharacterGender{ get; set; }
+        [Required]
+        public string CharacterMushroom{ get; set; }
+    }
+}
